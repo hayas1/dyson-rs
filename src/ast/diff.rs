59a51e4ca116b91f43f3cc6fc791c81b9560f4ed
hayas1 <h4ystack@gmail.com>
2022-09-11 01:30:28 +0900

    add diff with message function

--- src/ast/diff.rs
+++ src/ast/diff.rs
@@ -49,6 +49,30 @@ pub fn diff_value(a: &Value, b: &Value) -> Vec<(JsonPath, JsonPath)> {
     differences
 }
 
+/// **O(max{|a|, |b|})**, compare `a` and `b` that are expected same structure.
+/// with human friendly message.
+/// # panics
+/// if 'a' and 'b' do not have same structure.
+pub fn diff_value_detail(a: &Value, b: &Value) -> Vec<String> {
+    let mut result = Vec::new();
+    let path = diff_value(a, b);
+    for (pa, pb) in path {
+        if pa.last() == pb.last() {
+            result.push(format!("{:?}: different value {} and {}", pa, a[&pa], b[&pb]));
+        } else {
+            let (pal, (pbl, prefix)) =
+                (pa.last(), pb.split_last().map_or_else(|| (None, &[][..]), |(t, h)| (Some(t), h)));
+            match (pal, pbl) {
+                (Some(pal), Some(pbl)) => {
+                    result.push(format!("{:?}: different key {:?} and {:?}", prefix, pal, pbl));
+                }
+                _ => unreachable!("above function ensure that pa and pb have same length"),
+            }
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashSet;
@@ -92,5 +116,33 @@ mod tests {
         for (path1, path2) in diff_path {
             assert_ne!(ast_root1[&path1], ast_root2[&path2]);
         }
+    }
+
+    #[test]
+    fn test_diff_value_detail_json() {
+        let json1 = [
+            r#"{"#,
+            r#"    "language": "rust","#,
+            r#"    "notation": "json","#,
+            r#"    "version": 0.1,"#,
+            r#"    "keyword": ["rust", "json", "parser", 1, 2, 3]"#,
+            r#"}"#,
+        ];
+        let json2 = [
+            r#"{"#,
+            r#"    "language": "rust","#,
+            r#"    "notation": "json","#,
+            r#"    "version": 0.1,"#,
+            r#"    "keyword": ["ruby", "json", "parser", 1, 2, 3]"#,
+            r#"}"#,
+        ];
+        let ast_root1 = Value::parse(json1.into_iter().collect::<String>()).unwrap();
+        let ast_root2 = Value::parse(json2.into_iter().collect::<String>()).unwrap();
+
+        let diff = diff_value_detail(&ast_root1, &ast_root2);
+        assert!(diff[0].contains("keyword"));
+        assert!(diff[0].contains("0"));
+        assert!(diff[0].contains("rust"));
+        assert!(diff[0].contains("ruby"));
     }
 }
